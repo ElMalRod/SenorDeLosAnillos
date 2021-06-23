404fcec4932862a6d2c939382ee51cb886dc94c6
Emilio Maldonado <luismaldonado201931707@cunoc,edu,gt>
2021-06-22 20:17:21 -0600

    Agrego A Personajes (Heros y Bestias)

---
+++ src/senordelosanillos/SenordeLosAnillos.java
@@ -0,0 +1,17 @@
+package senordelosanillos;
+
+/**
+ *
+ * @author emili
+ */
+public class SenordeLosAnillos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Juego Señor de Los Anillos");
+    }
+    
+}
